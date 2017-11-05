ba215ebce912ec3e7cfdc2ffb4123715ca7d1717
Vishal Mohanan <VishalKayupravil.Kavumpravil@conduent.com>
2017-11-05 18:35:10 +0530

    Routing - links for member and product in main page without refreshing page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { ProductComponent }  from './product/product.component';
 import { MemberComponent }  from './members/members.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule,
+  	RouterModule.forRoot([{
+  	path:'member',
+  	component:MemberComponent
+  },{
+	path:'product',
+  	component:ProductComponent
+  }])],
   declarations: [ AppComponent,ProductComponent,MemberComponent ],
   bootstrap:    [ AppComponent ]
 })
