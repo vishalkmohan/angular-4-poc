a3de899a6a15f6addd4ef4d2584cf8bcd478d35b
Vishal Mohanan <VishalKayupravil.Kavumpravil@conduent.com>
2017-11-05 19:14:58 +0530

    Bootstrap table and data binding

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,6 +7,11 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent  { 
 	title:string = 'My Product'; 
+	productList=[{id:"10001",name:"Mobile",price:"10,000"},
+	{id:"10001",name:"Mobile",price:"10,000"},
+	{id:"10002",name:"Laptop",price:"15,000"},
+	{id:"10003",name:"TV",price:"12,000"},
+	{id:"10004",name:"Desktop",price:"13,000"}];
 }
 
 
