930e3f29826e7939fe867af5d6847172995d783c
Vishal Mohanan <VishalKayupravil.Kavumpravil@conduent.com>
2017-11-06 21:42:26 +0530

    added new component - converter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent }  from './app.component';
 import { ProductComponent }  from './product/product.component';
 import { MemberComponent }  from './members/members.component';
 import { ProducerComponent }  from './producer/producer.component';
+import { ConverterComponent }  from './converter/converter.component';
 import { SortPipe } from './app.sort';
 
 @NgModule({
@@ -23,8 +24,11 @@ import { SortPipe } from './app.sort';
   },{
   path:'producer',
     component:ProducerComponent
+  },{
+  path:'converter',
+    component:ConverterComponent
   }])],
-  declarations: [ SortPipe, AppComponent,ProductComponent,MemberComponent,ProducerComponent ],
+  declarations: [ SortPipe, AppComponent,ProductComponent,MemberComponent,ProducerComponent,ConverterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/converter/converter.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-converter',
+  templateUrl: './converter.component.html',
+  styleUrls: ['./converter.component.css']
+})
+export class ConverterComponent  { 
+	title:string = 'My Converter'; 
+
+}
+
+
